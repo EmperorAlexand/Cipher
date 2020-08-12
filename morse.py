94a31b5279c8e4fb80c41624b53f9fd069bac326
EmperorAlexand <55013991+EmperorAlexand@users.noreply.github.com>
2020-08-12 13:43:10 +0200

    Main file added
    Pog

---
+++ morse.py
@@ -0,0 +1,40 @@
+char_to_dots = {
+  'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.',
+  'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..',
+  'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.',
+  'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+  'Y': '-.--', 'Z': '--..', ' ': ' ', '0': '-----',
+  '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....',
+  '6': '-....', '7': '--...', '8': '---..', '9': '----.',
+  '&': '.-...', "'": '.----.', '@': '.--.-.', ')': '-.--.-', '(': '-.--.',
+  ':': '---...', ',': '--..--', '=': '-...-', '!': '-.-.--', '.': '.-.-.-',
+  '-': '-....-', '+': '.-.-.', '"': '.-..-.', '?': '..--..', '/': '-..-.'
+}
+
+dots_to_char = {v: k for k, v in char_to_dots.items()}
+
+def encode(x):
+    global char_to_dots
+    x = x.upper()
+    y = []
+    for i in range(len(x)):
+        y.append(char_to_dots[x[i]])
+    print(' '.join(y))
+
+
+def decode(x):
+    global dots_to_char
+    x = x.split()
+    y = []
+    for i in range(len(x)):
+        y.append(dots_to_char[x[i]])
+    print(''.join(y))
+
+while True:
+    z = input('Encode or decode morse? (E/D)\n')
+    if z == 'E':
+        omega = input('Encode:\n')
+        encode(omega)
+    elif z == 'D':
+        omega = input('Decode:\n')
+        decode(omega)
